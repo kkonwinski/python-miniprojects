8f58c7b105b727a041a4faf02683b93c47270393
Krzysztof Konwiński <k.konwinski@josera.pl>
2022-02-11 13:54:06 +0100

    Quiz Game

---
+++ quiz_game.py
@@ -0,0 +1,38 @@
+print("Welcome in my computer quiz!!!")
+
+playing = input("Do you want play? ")
+
+if playing.lower() != "yes":
+    quit()
+print("Okay! Lets play :)")
+score = 0
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "rando access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str(score/4 * 100) + "% questions correct!")
